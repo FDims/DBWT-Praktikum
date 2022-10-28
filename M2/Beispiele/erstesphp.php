b7ecd9e5bf7ee195a71f31de65d20fd56044eb84
FDims <fachrialdimaspp15@gmail.com>
2022-10-28 13:11:41 +0200

    Praktikum2 aufgabe 3 updated

--- M2/Beispiele/erstesphp.php
+++ M2/Beispiele/erstesphp.php
@@ -1,3 +1,8 @@
 <?php
+/**
+ * Praktikum DBWT. Autoren:
+ * Vorname1, Nachname1, Matrikelnummer1
+ * Vorname2, Nachname2, Matrikelnummer2
+ */
 echo "Erstes PHP Skript <br>";
 phpinfo();
--- M2/Beispiele/meal.php
+++ M2/Beispiele/meal.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * Praktikum DBWT. Autoren:
+ * Vorname1, Nachname1, Matrikelnummer1
+ * Vorname2, Nachname2, Matrikelnummer2
+ */
 const GET_PARAM_MIN_STARS = 'search_min_stars';
 const GET_PARAM_SEARCH_TEXT = 'search_text';
-
 /**
  * List of all allergens.
  */
@@ -9,7 +13,7 @@ $allergens = [
     11 => 'Gluten',
     12 => 'Krebstiere',
     13 => 'Eier',
-    14 => 'Fisch'
+    14 => 'Fisch',
     17 => 'Milch'
 ];
 
@@ -18,7 +22,7 @@ $meal = [
     'description' => 'Die Süßkartoffeln werden vorsichtig aufgeschnitten und der Frischkäse eingefüllt.',
     'price_intern' => 2.90,
     'price_extern' => 3.90,
-    'allergens' => [11, 13,
+    'allergens' => [11, 13],
     'amount' => 42             // Number of available meals
 ];
 
@@ -38,10 +42,11 @@ $ratings = [
 ];
 
 $showRatings = [];
-if (!empty($_GET[GET_PARAM_SEARCH_TEXT]) {
+
+if (!empty($_GET[GET_PARAM_SEARCH_TEXT])) {
     $searchTerm = $_GET[GET_PARAM_SEARCH_TEXT];
     foreach ($ratings as $rating) {
-        if (strpos($rating['text'], $searchTerm) !== false) {
+        if (strpos(strtolower($rating['text']), strtolower($searchTerm)) !== false) {
             $showRatings[] = $rating;
         }
     }
@@ -56,14 +61,27 @@ if (!empty($_GET[GET_PARAM_SEARCH_TEXT]) {
     $showRatings = $ratings;
 }
 
-calcMeanStars(array $ratings) : float {
-    $sum = 1;
+function calcMeanStars(array $ratings) : float {
+    $sum = 0;
     foreach ($ratings as $rating) {
-        $sum += $rating['stars'] / count($ratings);
+        $sum += $rating['stars'] ;
     }
+    $sum /=count($ratings);
     return $sum;
 }
 
+$Show_Description = 0;
+$style='hide';
+if(isset($_GET['show_description'])){
+    $Show_Description=$_GET['show_description'];
+    if($Show_Description==1){
+        $Show_Description=0;
+        $style='none';
+    }else if($Show_Description==0){
+        $Show_Description=1;
+        $style='block';
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -81,7 +99,15 @@ calcMeanStars(array $ratings) : float {
     </head>
     <body>
         <h1>Gericht: <?php echo $meal['name']; ?></h1>
-        <p><?php echo $meal['description']; ?></p>
+        <a href="<?php echo'?show_description='.$Show_Description;?>">Beschreibung ein/ausblenden</a>
+        <p style="display:<?php echo $style?> "><?php echo $meal['description']; ?></p>
+        <ul>
+            <?php
+            foreach ($meal['allergens']as $allergene){
+                echo "<li class='allergens'>$allergens[$allergene]</li>";
+            }
+            ?>
+        </ul>
         <h1>Bewertungen (Insgesamt: <?php echo calcMeanStars($ratings); ?>)</h1>
         <form method="get">
             <label for="search_text">Filter:</label>
@@ -92,6 +118,7 @@ calcMeanStars(array $ratings) : float {
             <thead>
             <tr>
                 <td>Text</td>
+                <td>Author</td>
                 <td>Sterne</td>
             </tr>
             </thead>
@@ -99,6 +126,7 @@ calcMeanStars(array $ratings) : float {
             <?php
         foreach ($showRatings as $rating) {
             echo "<tr><td class='rating_text'>{$rating['text']}</td>
+                      <td class='rating_author'>{$rating['author']}</td>
                       <td class='rating_stars'>{$rating['stars']}</td>
                   </tr>";
         }
