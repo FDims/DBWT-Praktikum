0fcd041b32e26b84f20b5e8bd81276b4cece93ae
FDims <110996703+FDims@users.noreply.github.com>
2022-11-03 22:31:26 +0100

    Update M2_7b_showtext.php

--- M2/Beispiele/M2_7b_showtext.php
+++ M2/Beispiele/M2_7b_showtext.php
@@ -1,3 +1,25 @@
+<?php
+$file = fopen('en.txt','r'); $gefunden = false;
+$fehler = false;
+$searched=NULL;
+$translate=NULL;
+if(isset($_GET['suche'])) {
+    $searched = $_GET['suche'];
+    while (!feof($file)) {
+        $line = fgets($file, 1024);
+        $word = explode(';',$line);
+        if (strtolower($word[0])==strtolower($searched)) {
+            $gefunden = true;
+            $translate=$word[1];
+        }
+    }
+    if (!$gefunden) {
+       $fehler= "Das gesuchte Wort" . '<suche>' . " ist nicht enthalten";
+    }
+}
+fclose($file);
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -12,23 +34,10 @@
 <body>
 <form method="get">
     <label for="suche">Suchen*</label><br>
-    <input type="text" name="suche" placeholder="Bitte geben Sie ein Wort" size="30" id="suche" required><br>
+    <input type="text" name="suche" placeholder="<?php echo !empty($searched)?$searched:'Bitte geben Sie ein Wort';?>" size="30" id="suche" required><br>
     <input type="submit" id="submit">
 </form>
+<?php  echo $fehler? $fehler:'English :'.$translate; ?>
 </body>
 </html>
 
-<?php
-$file = fopen('en.txt','r'); $gefunden = false;
-while (!feof($file)) {
-    $line = fgets($file, 1024);
-    if($_GET["suche"] == $line) {
-        echo fgets($file, 1024);
-        $gefunden = true;
-    }
-}
-if(!$gefunden){
-    echo "Das gesuchte Wort".'<suche>'." ist nicht enthalten";
-}
-fclose($file);
-?>
