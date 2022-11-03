64e38056c198bc80b807f4379edc54cfcad28a29
jerichojordan <jericho.jordan.ong@gmail.com>
2022-11-03 22:28:59 +0100

    A7a Done

--- M2/Beispiele/M2_7a_accesslog.php
+++ M2/Beispiele/M2_7a_accesslog.php
@@ -3,4 +3,9 @@ $file = fopen("accesslog.txt","w");
 if (!$file) {
     die('Öffnen fehlgeschlagen');
 }
-print_r(gettimeofday());
+date_default_timezone_set('Europe/Berlin');
+$datum = "Zeitzone : ".date_default_timezone_get()."\tDatum : ".date("d.m.y")."\tAktuelle Zeit : ".date("H:i:s");
+$webinfo1 = "\n\nServer Name : ".$_SERVER['SERVER_NAME']."\nServer IP Adresse : ".$_SERVER['SERVER_ADDR']."\nServer Port : ".$_SERVER['SERVER_PORT'];
+$webinfo2 = "\nClient IP Adresse : ".$_SERVER['REMOTE_ADDR']."\nServer Protokol : ".$_SERVER['SERVER_PROTOCOL'];
+fwrite($file,$datum);fwrite($file,$webinfo1);fwrite($file,$webinfo2);
+fclose($file);
