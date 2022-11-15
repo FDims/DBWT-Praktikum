b08df19b48497f80f1bb24ff00eba71ba49c40e4
FDims <110996703+FDims@users.noreply.github.com>
2022-11-15 13:43:27 +0100

    m3_4

--- M3/m3_41_testdatenbank.php
+++ M3/m3_41_testdatenbank.php
@@ -1,3 +1,28 @@
 <?php
-$mysqli = new mysqli("localhost","root","2804","emensawerbeseite",3306);
-echo $mysqli->host_info."\n";
+echo "test";
+$link = mysqli_connect(
+    "localhost", // Host der Datenbank
+    "root", // Benutzername zur Anmeldung
+    "2804", // Passwort zur Anmeldung
+    "emensawerbeseite", // Auswahl der Datenbank
+    3306);
+echo "test";
+if (!$link) {
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+
+$sql = "SELECT id, name, beschreibung FROM gericht";
+
+$result = mysqli_query($link, $sql);
+if (!$result) {
+    echo "Fehler während der Abfrage:  ", mysqli_error($link);
+    exit();
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo '<li>'.$row['id']. ':'. $row['name']. '</li>';
+}
+
+mysqli_free_result($result);
+mysqli_close($link);
