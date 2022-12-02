2e79b5acc57cdc8ea0d00a7d18c1483cdb2f50fa
jerichojordan <jericho.jordan.ong@gmail.com>
2022-12-02 15:28:35 +0100

    M4

--- M4/emensa/models/gericht.php
+++ M4/emensa/models/gericht.php
@@ -33,3 +33,26 @@ function db_gericht_preisintern(){
     mysqli_close($link);
     return $data;
 }
+function db_gericht_tabelle(){
+$link = connectdb();
+$sql1= "SELECT id, name, preis_intern , preis_extern FROM gericht ORDER BY name ASC ";
+$result1= mysqli_query($link,$sql1);
+$sql2= "SELECT gericht_id, code FROM gericht_hat_allergen";
+$result2=mysqli_query($link,$sql2);
+for($i=0;$i<5;$i++){
+    $line=null;
+    $row1=mysqli_fetch_assoc($result1);
+    echo '<tr>';
+    echo '<td class="menulist">'.$row1['name'].'<br>';
+    $sql2= "SELECT gericht_id, code FROM gericht_hat_allergen WHERE gericht_id=".$row1['id'];
+    $result2=mysqli_query($link,$sql2);
+    while($row2=mysqli_fetch_assoc($result2)){
+        $line=$line.$row2['code'].', ';
+    }
+    if($line) {
+        echo '[' . $line . ']';
+    }
+    echo '</td>';
+    echo '<td class="menulist">'.number_format($row1['preis_intern'],2,',','.').'</td>';
+    echo '<td class="menulist">'.number_format($row1['preis_extern'],2,',','.').'</td>';
+}}
--- werbeseite/wunschgericht.php
+++ werbeseite/wunschgericht.php
@@ -5,7 +5,7 @@ $datum =date("d.m.Y");;
 $link = mysqli_connect(
     "localhost", // Host der Datenbank
     "root", // Benutzername zur Anmeldung
-    "2804", // Passwort zur Anmeldung
+    "root", // Passwort zur Anmeldung
     "emensawerbeseite", // Auswahl der Datenbank
     3306);
 if (!$link) {
