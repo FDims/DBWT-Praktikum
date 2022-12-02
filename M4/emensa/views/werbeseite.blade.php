9ced17fa03f7950bcfaea130c46ef2db331b216e
jerichojordan <jericho.jordan.ong@gmail.com>
2022-12-02 20:01:17 +0100

    M4

--- M4/emensa/views/werbeseite.blade.php
+++ M4/emensa/views/werbeseite.blade.php
@@ -15,42 +15,10 @@
                 no sea takimata sanctus est Lorem ipsum dolor sit amet.</p></fieldset>
         <h2 id="speisen">Köstlichkeiten, die Sie erwarten</h2>
         <table id="menu">
-                <thead>
-                <th>Gericht</th>
-                <th>Preis intern</th>
-                <th>Preis extern</th>
-                <th>Allergen</th>
-                </thead>
-                <tbody>
-                @foreach($gericht as $key)
-                    <tr>
-                        <td>{{$key['name']}}</td>
-                        <td>{{number_format($key['preis_intern'],2)}}</td>
-                        <td>{{number_format($key['preis_extern'],2)}}</td>
-                        @if(isset($key['code']))
-                            <td>{{$key['code']}}</td>
-                        @endif
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            <h3>Folgende Allergencodes enthalten:</h3>
-            <table>
-                <thead>
-                <tr>
-                    <td>Code</td>
-                    <td>Allergen</td>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($allergen as $key)
-                    <tr>
-                        <td>{{$key['code']}}</td>
-                        <td>{{$key['name']}}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+
+
+
+        </table>
         </div>
     </div>
 @endsection
